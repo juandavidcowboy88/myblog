cc7651ebfb823dfda5f64b93a1c84cabe383ee15
juandavidcowboy88 <ing.juandavid.alvarez@gmail.com>
2017-03-22 11:28:27 -0500

    sebas help 2

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -5,22 +5,21 @@ class UserNotifierMailer < ApplicationMailer
   #
   #   en.user_notifier_mailer.welcome_user.subject
   #
-  def welcome_user
-    @user = user
-    @url = 'https://blog-makeit-juan.herokuapp.com/posts'
-    
-    mail(to: @user.email, subject: 'wilkomme makkereal camp')
-  end
+    def welcome_user
+      @user = user
+      @url = 'https://blog-makeit-juan.herokuapp.com/posts'
+
+      mail(to: @user.email, subject: 'wilkomme makkereal camp')
+    end
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_notifier_mailer.new_post_mail.subject
   #
-  def new_post_mail
+    def new_post_mail
     @user = user
     @post = post
-
-    mail(to: @user.email, subject 'Un nuevo post ha sido creado ')
-  end
+    mail(to: @user.email, subject: 'Un nuevo post ha sido creado ')
+    end
 end
